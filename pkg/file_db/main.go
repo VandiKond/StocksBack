11c63abe9eef53ec981a28ffbe431e52265abc9d
Lev Vandi <Lev-person11@yandex.ru>
2024-12-03 18:39:51 +0300

    finishing file db

--- pkg/file_db/main.go
+++ pkg/file_db/main.go
@@ -11,19 +11,34 @@ import (
 
 // The errors
 const (
+	ErrorOpeningFile  = "error opening file"  // error opening file
 	ErrorCreatingData = "error creating data" // error creating data
 	ErrorEncodingData = "error encoding data" // error encoding data
 	ErrorDecodingData = "error decoding data" // error encoding data
 	InvalidQuery      = "invalid query"       // invalid query
+	InvalidId         = "invalid id"          // invalid id
 )
 
 type FileDB struct {
 	*os.File
+	data []user_cfg.User
+}
+
+func NewFileDB(fn string) (*FileDB, error) {
+	file, err := os.OpenFile(fn, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, vanerrors.NewWrap(ErrorOpeningFile, err, vanerrors.EmptyHandler)
+	}
+	return &FileDB{
+		File: file,
+		data: []user_cfg.User{},
+	}, nil
 }
 
 func (db *FileDB) Create() error {
 	usrArr := []user_cfg.User{}
 	err := json.NewDecoder(db).Decode(&usrArr)
+	db.data = usrArr
 	if err != nil {
 		err := json.NewEncoder(db).Encode(usrArr)
 		if err != nil {
@@ -34,44 +49,28 @@ func (db *FileDB) Create() error {
 }
 
 func (db *FileDB) NewUser(usr user_cfg.User) error {
-	usrArr := []user_cfg.User{}
-	err := json.NewDecoder(db).Decode(&usrArr)
-	if err != nil {
-		return vanerrors.NewWrap(ErrorDecodingData, err, vanerrors.EmptyHandler)
-	}
+	usrArr := db.data
 
 	usrArr = append(usrArr, usr)
-	err = json.NewEncoder(db).Encode(usrArr)
+	err := json.NewEncoder(db).Encode(usrArr)
 	if err != nil {
 		return vanerrors.NewWrap(ErrorEncodingData, err, vanerrors.EmptyHandler)
 	}
+	db.data = usrArr
 	return nil
 }
 
 func (db *FileDB) GetAll() ([]user_cfg.User, error) {
-	usrArr := []user_cfg.User{}
-	err := json.NewDecoder(db).Decode(&usrArr)
-	if err != nil {
-		return nil, vanerrors.NewWrap(ErrorDecodingData, err, vanerrors.EmptyHandler)
-	}
-	return usrArr, nil
+	return db.data, nil
 }
 
 func (db *FileDB) Select(uid uint64) (*user_cfg.User, error) {
-	usrArr := []user_cfg.User{}
-	err := json.NewDecoder(db).Decode(&usrArr)
-	if err != nil {
-		return nil, vanerrors.NewWrap(ErrorDecodingData, err, vanerrors.EmptyHandler)
-	}
+	usrArr := db.data
 	return &usrArr[uid], nil
 }
 
 func (db *FileDB) SelectNumBy(q any, num int) ([]user_cfg.User, error) {
-	usrArr := []user_cfg.User{}
-	err := json.NewDecoder(db).Decode(&usrArr)
-	if err != nil {
-		return nil, vanerrors.NewWrap(ErrorDecodingData, err, vanerrors.EmptyHandler)
-	}
+	usrArr := db.data
 	if num == -1 {
 		num = len(usrArr) - 1
 	}
@@ -149,4 +148,41 @@ func (db *FileDB) SelectOneBy(q any) (*user_cfg.User, error) {
 	}
 
 	return &res[0], nil
+}
+
+func (db *FileDB) Update(usr user_cfg.User) error {
+	usrArr := db.data
+
+	if len(usrArr) >= int(usr.Id) {
+		return vanerrors.NewSimple(InvalidId)
+	}
+	usrArr[usr.Id] = usr
+
+	err := json.NewEncoder(db).Encode(usrArr)
+	if err != nil {
+		return vanerrors.NewWrap(ErrorEncodingData, err, vanerrors.EmptyHandler)
+	}
+	db.data = usrArr
+	return nil
+}
+
+func (db *FileDB) UpdateGroup(users []user_cfg.User) error {
+	usrArr := db.data
+	for _, usr := range users {
+		if len(usrArr) >= int(usr.Id) {
+			return vanerrors.NewSimple(InvalidId)
+		}
+		usrArr[usr.Id] = usr
+	}
+
+	err := json.NewEncoder(db).Encode(usrArr)
+	if err != nil {
+		return vanerrors.NewWrap(ErrorEncodingData, err, vanerrors.EmptyHandler)
+	}
+	db.data = usrArr
+	return nil
+}
+
+func (db *FileDB) GetLast() (uint64, error) {
+	return db.data[len(db.data)-1].Id, nil
 }
