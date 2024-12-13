7f7fb563a8db8cf02ff41b1e6e4ada2fdb3935f6
Lev Vandi <Lev-person11@yandex.ru>
2024-12-13 21:21:59 +0300

    adding logs directory and config from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// Creating a new application with a hour timeout
-	app := application.New()
+	app := application.New("config/config.yml")
 
 	// Adding graceful shutdown
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
---
+++ logs/logs.md
@@ -0,0 +1,6 @@
+# This is the logs folder
+
+## Why is it essential?
+
+1. The logs would be stored here
+2. Git will remove the directory if there would be no files
